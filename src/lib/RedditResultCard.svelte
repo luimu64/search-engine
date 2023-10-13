96cb18998d655eb2d140655cbb3d13001d7a415a
luimu64 <luimu@luimu.dev>
2023-10-13 23:08:53 +0300

    added nice icons on cards

--- src/lib/RedditResultCard.svelte
+++ src/lib/RedditResultCard.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   ///<reference path="../sources/reddit.d.ts" />
+  import Icon from "@iconify/svelte";
+
   export let result: IRedditSearchResult;
 </script>
 
@@ -9,7 +11,16 @@
   <h2 class="text-2xl"><a href={result.newUrl}>{result.title}</a></h2>
   <p>{result.body}</p>
   <div class="flex gap-2">
-    <div>Comments: {result.comments}</div>
-    <div>Upvotes: {result.upvotes}</div>
+    <div class="flex items-center gap-1">
+      <div>
+        <Icon icon="bxs:up-arrow" class="text-xs" />
+        <Icon icon="bxs:down-arrow" class="text-xs" />
+      </div>
+      {result.upvotes}
+    </div>
+    <div class="flex items-center gap-1">
+      <Icon icon="fluent:comment-24-regular" />
+      {result.comments}
+    </div>
   </div>
 </div>
--- src/lib/StackOverflowResultCard.svelte
+++ src/lib/StackOverflowResultCard.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   ///<reference path="../sources/stackOverflow.d.ts" />
+  import Icon from "@iconify/svelte";
+
   export let result: IStackOverflowSearchResult;
 </script>
 
@@ -7,9 +9,20 @@
   class="border-l-4 border-orange-600 bg-[--primary] rounded p-5 text-[--text] flex flex-col gap-2 shadow-lg"
 >
   <h2 class="text-2xl"><a href={result.link}>{result.title}</a></h2>
-  <div class="flex gap-2">
-    <div>Answered: {result.is_answered}</div>
-    <div>Answer count: {result.answer_count}</div>
-    <div>score: {result.score}</div>
+  <div class="flex gap-2 items-center">
+    <div class="flex items-center gap-1">
+      <div>
+        <Icon icon="bxs:up-arrow" class="text-xs" />
+        <Icon icon="bxs:down-arrow" class="text-xs" />
+      </div>
+      {result.score}
+    </div>
+    <div class="flex items-center gap-1">
+      <Icon icon="fluent:comment-24-regular" />
+      {result.answer_count}
+    </div>
+    {#if result.is_answered}
+      <Icon icon="material-symbols:check" class="text-2xl" />
+    {/if}
   </div>
 </div>
